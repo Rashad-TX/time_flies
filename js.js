b5ef043f8dfa103d73141ace7c5bb98b8c33a1bb
Rashad Moore <rjm4god7@gmail.com>
2022-06-04 09:43:40 -0500

    capture submission to object

--- js.js
+++ js.js
@@ -1,15 +1,16 @@
-class progress {
-    constructor(category, name){
-        this.category = "category";
-        this.name = "name";
+const subBtn = document.getElementById("sub-btn").addEventListener("click",addSubmission);
+const submission= [];
+
+function addSubmission(event){
+    event.preventDefault();
+    const progress = {
+        id:Date.now(),
+        category: document.getElementById("category").value,
+        name: input2 = document.getElementById("task").value
     }
+    submission.push(progress);
+    document.querySelector("form").reset();
 }
 
-class task {
-    constructor(category, name, time){
-        this.category = "category";
-        this.name = "name";
-        this.time = "time"
 
-    }
-}
+
