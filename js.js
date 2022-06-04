9f32e32e7d139a0e879438d019c8033849f3db5d
Rashad Moore <rjm4god7@gmail.com>
2022-06-03 23:51:17 -0500

    nav and input

---
+++ js.js
@@ -0,0 +1,15 @@
+class progress {
+    constructor(category, name){
+        this.category = "category";
+        this.name = "name";
+    }
+}
+
+class task {
+    constructor(category, name, time){
+        this.category = "category";
+        this.name = "name";
+        this.time = "time"
+
+    }
+}
