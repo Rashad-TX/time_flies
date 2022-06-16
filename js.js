a358e0b699524b307206b3b41862f45d5fd75a77
Rashad Moore <rjm4god7@gmail.com>
2022-06-16 05:29:47 -0500

    updated start and stop functionality

--- js.js
+++ js.js
@@ -1,16 +1,13 @@
+
+
 //display current date
 let date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
 let showDate = document.getElementById("insert-date");
 showDate.innerText=date;
 
-//Global Variables for timer function
-let timer;
-let sec = 0;
-let min = 0;
-let hour = 0
-let newElapsedTime;
 
-//Get number of buttons for loop to add event listener
+
+//Get number of activity buttons for loop to add event listener to each
 const btnLength = document.querySelectorAll(".opt-btn").length;
 
 //Will append new div's here
@@ -21,83 +18,62 @@ const mainDiv = document.getElementById("activities");
 document.querySelectorAll(".opt-btn")[i].addEventListener("click", function() {
     const buttonValue = this.value;
     addActivity(buttonValue);
-})
-    }
+});
+   
 
-    //Onclick event creates the div, li, i, p , play, stop, timer text and appends to the main div
+    //Onclick event creates the div, i, p , play, stop, timer text and appends to the main div
 function addActivity(buttonValue){
  
  
         const newDiv = document.createElement('div');
      newDiv.classList.add("activity");
 
-     const newLi = document.createElement('li');
-     newLi.innerText = buttonValue;
-     newLi.classList.add("act-li");
-     newDiv.appendChild(newLi);
+     const newP = document.createElement('p');
+     newP.innerText = buttonValue;
+     newP.classList.add("act-p");
+     newDiv.appendChild(newP);
 
      const newPlay =document.createElement('i');
      newPlay.classList.add("fa-solid");
      newPlay.classList.add("fa-play");
-     newPlay.addEventListener("click",function(e){
-        
-     timer = setInterval(startTimer, 1000);
-     })
-    newDiv.appendChild(newPlay);
+     let activityInterval;
+     newPlay.addEventListener('click', () => {
+     activityInterval = setInterval(() => {
+            elapsedSeconds++
+            updateTimerValue()
+          }, 1000)
+        })
+       newDiv.appendChild(newPlay);
 
     const newStop =document.createElement('i');
     newStop.classList.add("fa-solid");
     newStop.classList.add("fa-circle-stop");
-    newStop.addEventListener("click", stopTimer)
+    newStop.addEventListener("click", () =>clearInterval(activityInterval));
     newDiv.appendChild(newStop);
 
-    newElapsedTime = document.createElement('p');
+    const newElapsedTime = document.createElement('p');
     newElapsedTime.classList.add("elapsedTime-Text");
     newElapsedTime.innerText = "00:00:00";
     newDiv.appendChild(newElapsedTime);
+    mainDiv.appendChild(newDiv);
 
-          mainDiv.appendChild(newDiv);
-    }
+    let elapsedSeconds = 0;
+    updateTimerValue()
 
-    function startTimer(e) {
-        sec++;
-        if(sec === 60){
-            sec === 0
-            min++;
-        }
-    if (min === 60){
-        min === 0;
-        hour++;
-        
-    }
-    displayTime(e)
+
+    function updateTimerValue() {
+        // Count the seconds, minutes and hours, convert to strings and pad the strings with leading 0's
+        const seconds = String(elapsedSeconds % 60).padStart(2, '0')
+        const minutes = String(parseInt(elapsedSeconds / 60 % 60)).padStart(2, '0')
+        const hours = String(parseInt(elapsedSeconds / 60 / 60)).padStart(2, '0')
     
+        // Use string interpolation for formatting the timer string
+        newElapsedTime.innerText = `${hours}:${minutes}:${seconds}`
+      }
+
+                 
+
+
     }
 
-    function displayTime(e){
-        let sec_format = sec;
-        let min_format = min;
-        let hour_format = hour;
-     
-    
-      
-        if(sec < 10){
-            sec_format = "0"+sec;
-        }
-        if(min < 10){
-            min_format = "0"+min;
-        }
-        if (hour <10){
-            hour_format = "0"+hour;
-        }
-        newElapsedTime.innerHTML = hour_format + " :" + min_format +" : " + sec_format;
-    }
-
-
-
-     
-
-function stopTimer(){
-    clearInterval(startTimer);
-    console.log("pressed");
-}
+ }
